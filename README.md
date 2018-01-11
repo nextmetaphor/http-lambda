9c54a788c322be52ac06af6cb239f944b6e3f102
Paul Tatham <paul@nextmetaphor.com>
2018-01-11 20:50:06 +0000

    initial working version

---
+++ README.md
@@ -0,0 +1,6 @@
+```bash
+gvt fetch --revision v1.12.59 github.com/aws/aws-sdk-go
+gvt fetch --revision v1.0.0 github.com/sirupsen/logrus
+gvt fetch --revision v1.6.0 github.com/gorilla/mux
+gvt fetch --revision v1.3.0 github.com/gorilla/handlers
+```
---
+++ http-lambda.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	"github.com/gorilla/handlers"
+	"context"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"io/ioutil"
+)
+
+const (
+	logServerStarted  = "http-lambda server starting on address [%s] and port [%d]"
+	logFunctionCalled = "Function %s called"
+	logSIGTERMReceived = "syscall.SIGTERM received, shutting down server"
+	logSignalReceived = "Signal [%s] received, shutting down server"
+
+	urlFunctionParameter = "function-name"
+	urlFunction          = "/function/{" + urlFunctionParameter + "}"
+
+	// TODO these should be read from command-line or config or both
+	cfgListenAddress = ""
+	cfgListenPort = 18080
+)
+
+func lambdaRequest(writer http.ResponseWriter, request *http.Request) {
+	// refer to
+	// https://github.com/awsdocs/aws-doc-sdk-examples/blob/master/go/example_code/lambda/aws-go-sdk-lambda-example-run-function.go
+
+	logger := logrus.New()
+
+	functionName := mux.Vars(request)[urlFunctionParameter]
+
+	logger.Infof(logFunctionCalled, functionName)
+
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	client := lambda.New(sess, &aws.Config{Region: aws.String(*sess.Config.Region)})
+
+	payload, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		logger.Error(err)
+		return
+	}
+
+	result, err := client.Invoke(&lambda.InvokeInput{FunctionName: aws.String(functionName), Payload: payload})
+	if err != nil {
+		logger.Error(err)
+	}
+
+	writer.WriteHeader(int(*result.StatusCode))
+	writer.Write(result.Payload)
+}
+
+func main() {
+	logger := logrus.New()
+
+	logger.Infof(logServerStarted, cfgListenAddress, cfgListenPort)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc(urlFunction, lambdaRequest).Methods(http.MethodPost)
+
+	server := &http.Server{
+		Addr:    cfgListenAddress + ":" + strconv.Itoa(cfgListenPort),
+		Handler: handlers.LoggingHandler(os.Stdout, router),
+	}
+
+	// See https://en.wikipedia.org/wiki/Signal_(IPC)
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	go func() {
+		s := <-signalChannel
+		switch s {
+		case syscall.SIGTERM:
+			logger.Info(logSIGTERMReceived)
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			server.Shutdown(ctx)
+
+		default:
+			logger.Infof(logSignalReceived, s)
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			server.Shutdown(ctx)
+		}
+
+	}()
+
+	logger.Info(server.ListenAndServe())
+}
