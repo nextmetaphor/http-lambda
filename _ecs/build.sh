c98be53a1c8d99a3d667671bf035c8fb66289aa1
Paul Tatham <paul@nextmetaphor.com>
2018-01-22 07:41:34 +0000

    addition of initial ECS files

--- _ecs/build.sh
+++ _ecs/build.sh
@@ -6,4 +6,8 @@ export GOARCH=amd64
 go build -i ../http-lambda.go
 docker build -t nextmetaphor/ecs-http-lambda:latest .
 
+# docker push nextmetaphor/ecs-http-lambda:latest
+
 docker run -d --name=ecs-http-lambda -p 18080:18080 nextmetaphor/ecs-http-lambda:latest
+
+aws cloudformation create-stack --stack-name ecs-http-lambda --template-body file://ecs.yaml --disable-rollback --parameters file://ecs-parameters.json --capabilities CAPABILITY_IAM
